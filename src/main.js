b552840979d578dac11b9414e25a332f638422b4
tipalol <tipalol2@gmail.com>
2024-01-08 14:22:48 +0300

    Revert "123123123"
    This reverts commit d8382fd6d47c626ba9a45a3469f93666ccccd541.
    
    (cherry picked from commit 0a71cc59e176c151e1a6624ab97b60d421488de7)

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import Router from './app/Router'
-import './index.css'
-import App from './app/App.vue'
-const app = createApp(App)
-app.use(Router)
-app.mount('#app')
--- src/shared/paragraph/paragraph.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <p class="mt-[40px] text-[16px] text-[#6C6C6C] leading-[26px] font-open"><slot></slot></p>
-</template>
-
-<script setup></script>
