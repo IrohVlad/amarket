777e27f250f480c82b7e4f552c40a60803cf829e
IrohVlad1 <iron_vlad80@mail.ru>
2024-01-18 21:20:06 +0300

    Shipment cards

--- app/shipment/page.tsx
+++ app/shipment/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ServiceHero from '../components/serviceHero/serviceHero'
 import ShipmentForm from './shipmentForm/shipmentForm'
+import ShipmentCards from './shipmentCards/shipmentCards'
+import Trucks from '../components/trucks/trucks'
 
 export default function Shipment() {
   return (
     <>
         <ServiceHero routes={['Главная', 'Оформить отправление']} title='Оформить отправление' img_url='/shipment/paper-box-delivery-man-in-uniform-is-indoors-with-2021-12-27-22-09-39-utc1.png' />
         <ShipmentForm/>
+        <ShipmentCards/>
+        <Trucks/>
     </>
   )
 }
