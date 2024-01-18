55ca3742a46b2da2b7c885cb990092fb7453c80a
IrohVlad1 <iron_vlad80@mail.ru>
2024-01-18 20:33:50 +0300

    Shipment form

---
+++ app/shipment/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServiceHero from '../components/serviceHero/serviceHero'
+import ShipmentForm from './shipmentForm/shipmentForm'
+
+export default function Shipment() {
+  return (
+    <>
+        <ServiceHero routes={['Главная', 'Оформить отправление']} title='Оформить отправление' img_url='/shipment/paper-box-delivery-man-in-uniform-is-indoors-with-2021-12-27-22-09-39-utc1.png' />
+        <ShipmentForm/>
+    </>
+  )
+}
