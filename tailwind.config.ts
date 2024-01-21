de2110c060e801dbe74ca0d9dc6ae47cf9694d9c
Peter Griffin <evertmind@gmail.com>
2024-01-21 23:32:07 +0300

    Header addaptive

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,17 +25,14 @@ const config: Config = {
       // => @media (max-width: 1535px) { ... }
       '1.5xl': {'max': '1350px'},
       // => @media (max-width: 1330px) { ... }
-
       'xl': {'max': '1279px'},
       // => @media (max-width: 1279px) { ... }
       '2lg': {'max': '1143px'},
       // => @media (max-width: 1023px) { ... }
       'lg': {'max': '1023px'},
       // => @media (max-width: 1023px) { ... }
-
       'md': {'max': '767px'},
       // => @media (max-width: 767px) { ... }
-
       'sm': {'max': '639px'},
       // => @media (max-width: 639px) { ... }
       'ss': {'max': '480px'},
