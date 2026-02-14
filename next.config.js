49a3fa4988ef83d740aa1dd7f939de4b00a260af
IrohVlad1 <iron_vlad80@mail.ru>
2026-02-14 20:43:43 +0300

    a

--- next.config.js
+++ next.config.js
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'my-apple-store.ru',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn.fishki.net',
-                port: '',
-                pathname: '**',
-            },
-        ],
-    },
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  output: "export",
+  basePath: "/visit_example/",
+  assetPrefix: "/visit_example/",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "my-apple-store.ru",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.fishki.net",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
